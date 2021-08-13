333e5d1fb2f6e065d935ae6a5309dd18aca7cc16
Ronnie Schaniel <ronnieschaniel@gmail.com>
2021-08-13 10:36:34 +0200

    multiple subscriptions on observable

--- src/2_observable/random_number_generator.spec.ts
+++ src/2_observable/random_number_generator.spec.ts
@@ -16,4 +16,23 @@ describe('Random Number Generator', () => {
             done();
         }, 2_500);
     });
+
+    it('should handle multiple observers', (done) => {
+        const randomNumbers1: number[] = [];
+        const randomNumbers2: number[] = [];
+
+
+        const subscription1 = randomNumberGenerator$.subscribe({ next: (x) => randomNumbers1.push(x) });
+        const subscription2 = randomNumberGenerator$.subscribe({ next: (x) => randomNumbers2.push(x) });
+
+        setTimeout(() => {
+            subscription1.unsubscribe();
+            subscription2.unsubscribe();
+            console.log(randomNumbers1);
+            console.log(randomNumbers2);
+            expect(randomNumbers1.length).toEqual(3);
+            expect(randomNumbers2.length).toEqual(3);
+            done();
+        }, 3_500);
+    });
 });
