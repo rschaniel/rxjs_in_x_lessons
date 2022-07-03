68abb69584a5c23a4db4e503d69a742a0fb8b603
Ronnie Schaniel <ronnieschaniel@gmail.com>
2022-07-03 15:50:58 +0200

    some lessons

---
+++ src/26_zip/zip.spec.ts
@@ -0,0 +1,25 @@
+import { of, Observable, throwError, asyncScheduler } from 'rxjs';
+import { BehaviorSubject, zip, interval } from 'rxjs';
+import { testScheduler } from '../misc/test_scheduler';
+import { take } from 'rxjs/operators';
+
+
+describe('forkJoin', () => {
+
+    it('marbles testing', () => {
+        testScheduler.run((helpers) => {
+            const { expectObservable } = helpers;
+
+            const concatResult$ = zip(
+                interval(1000).pipe(take(3)),
+                of(3, 4),
+            );
+
+            expectObservable(concatResult$).toBe(
+                '1s a 999ms b 999ms (cde|)',
+                { a: 0, b: 1, c: 2, d: 3, e: 4}
+            );
+
+        });
+    });
+});
